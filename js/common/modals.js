4eb07bbb4edc9486d557a14a78b5381bc5e2cdc6
Александр Конев <aleksandrkonev@MacBook-Pro-Aleksandr.local>
2024-02-29 14:53:45 +0200

    Cube game - 1 version

--- js/common/modals.js
+++ js/common/modals.js
@@ -1,17 +1,35 @@
+// Открытие модального окна
 document.querySelectorAll(".modal-trigger").forEach((trigger) => {
   trigger.addEventListener("click", () => {
     const modalId = trigger.dataset.target;
     const modal = document.getElementById(modalId);
-    // Открыть модальное окно
-    modal.classList.add("active");
+    modal.style.display = "block";
+
+    // Закрытие модального окна при щелчке вне его области
+    window.addEventListener("click", outsideClickHandler);
   });
 });
 
 // Закрытие модального окна
 document.querySelectorAll(".modal-close").forEach((closeBtn) => {
-  closeBtn.addEventListener("click", () => {
-    const modal = closeBtn.closest(".modal");
-    // Закрыть модальное окно
-    modal.classList.remove("active");
-  });
+  closeBtn.addEventListener("click", closeModal);
 });
+
+// Функция для закрытия модального окна
+function closeModal() {
+  document
+    .querySelectorAll(".modal")
+    .forEach((modal) => (modal.style.display = "none"));
+
+  // Удаление обработчика события для закрытия модального окна при щелчке вне его области
+  window.removeEventListener("click", outsideClickHandler);
+}
+
+// Обработчик события для закрытия модального окна при щелчке вне его области
+function outsideClickHandler(event) {
+  const { target } = event;
+  const modalContent = target.closest(".modal-content");
+  if (!modalContent && !target.classList.contains("modal-trigger")) {
+    closeModal();
+  }
+}
---
+++ js/games/crazyCube.js
@@ -0,0 +1,23 @@
+const cube = document.querySelector(".cube");
+
+function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function showDiceResult(number) {
+  console.log("The dice rolled:", number);
+  // Добавьте сюда логику для отображения результата на кубике
+}
+
+function startAnimation() {
+  cube.classList.add("animate");
+}
+
+function stopAnimation() {
+  cube.classList.remove("animate");
+}
+
+document.getElementById("startButton").addEventListener("click", () => {
+  startAnimation();
+  // setTimeout(stopAnimation, 10000);
+});
