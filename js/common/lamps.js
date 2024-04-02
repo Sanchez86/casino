7116694b7be8cf727018d059570862789e5af9cb
Александр Конев <aleksandrkonev@MacBook-Pro-Aleksandr.local>
2024-04-02 13:34:23 +0300

    Fixed bug with lamps

--- js/common/lamps.js
+++ js/common/lamps.js
@@ -1,12 +1,8 @@
 const switcher = document.getElementById("on-off");
 const lampSizePx = 26; // Размер лампочки в пикселях
-let isBlinking = true; // Переменная состояния мигания
+let isBlinking = true; // Состояние мигания
 
-// Функция для расчета количества лампочек в контейнере
-const countingLampsOnScreen = (containerSize, lampSize) =>
-  Math.floor(containerSize / lampSize);
-
-// Функция для создания лампочки
+// Создание лампочки
 const createLamp = () => {
   const lamp = document.createElement("div");
   lamp.classList.add("lamp");
@@ -15,9 +11,7 @@ const createLamp = () => {
 
 // Функция для создания лампочек в контейнере
 const createLamps = (container, lampCount) => {
-  for (let i = 0; i < lampCount; i++) {
-    container.appendChild(createLamp());
-  }
+  Array.from({ length: lampCount }, () => container.appendChild(createLamp()));
   startBlinking(container); // Запускаем мигание
 };
 
@@ -31,15 +25,16 @@ const startBlinking = (container) => {
     const intervalId = setInterval(() => {
       toggleLampClass(lamp);
     }, Math.random() * 1000 + 500); // Мигаем каждую лампочку с разной частотой (от 0.5 до 1.5 секунд)
-    container.intervalIds.push(intervalId); // Сохраняем идентификатор интервала в контейнере
+    container.intervalIds.push(intervalId); // Сохраняем идентификатор интервала в контейнере для того, что бы потом его очистить после остановки
   });
 };
 
 // Функция для остановки мигания лампочек в контейнере
 const stopBlinking = (container) => {
   container.intervalIds.forEach((intervalId) => clearInterval(intervalId));
-  const lamps = container.querySelectorAll(".lamp");
-  lamps.forEach((lamp) => lamp.classList.remove("on"));
+  container
+    .querySelectorAll(".lamp")
+    .forEach((lamp) => lamp.classList.remove("on"));
   container.intervalIds = []; // Очищаем массив идентификаторов интервалов в контейнере
 };
 
@@ -56,9 +51,9 @@ switcher.addEventListener("click", function () {
   isBlinking = !isBlinking; // Инвертируем переменную состояния мигания
 });
 
-// Определение размеров экрана
-const screenWidth = window.screen.width;
-const screenHeight = window.screen.height;
+// Определение размеров основного контейнера. Пока что сделал фиксированное
+const screenWidth = 900;
+const screenHeight = 380;
 
 // Получение контейнеров для лампочек
 const lampsContainers = document.querySelectorAll(
@@ -67,16 +62,12 @@ const lampsContainers = document.querySelectorAll(
 
 // Создание лампочек в каждом контейнере
 lampsContainers.forEach((container) => {
-  let containerSize;
-  if (
+  const containerSize =
     container.classList.contains("lampsTop") ||
     container.classList.contains("lampsBottom")
-  ) {
-    containerSize = screenWidth - 220;
-  } else {
-    containerSize = screenHeight - 420;
-  }
-  const lampCount = countingLampsOnScreen(containerSize, lampSizePx);
+      ? screenWidth
+      : screenHeight;
+  const lampCount = Math.floor(containerSize / lampSizePx);
   container.intervalIds = []; // Создаем массив для хранения идентификаторов интервалов в контейнере
   createLamps(container, lampCount);
 });
